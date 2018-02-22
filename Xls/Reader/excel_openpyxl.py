f8d8084b423b21dc2adb9670c7ad25d7b7bcd2ff
Maksim Sapyorov <lnglr69@gmail.com>
2018-02-22 17:27:14 +0200

    fix openpyxl empty row detection

--- Xls/Reader/excel_openpyxl.py
+++ Xls/Reader/excel_openpyxl.py
@@ -55,15 +55,18 @@ def run(argv):
         for row in sheet.iter_rows(row_offset=int(args.start) - 1):
             row_process_number += 1
             current_row_read = []
+            row_not_empty = False
             for cell in row:
                 value = cell.value
+                if value is not None:
+                    row_not_empty = True
                 if isinstance(value, (datetime.datetime, datetime.time)):
                     current_row_read.append(value.isoformat())
                 elif isinstance(value, (str, unicode)):
                     current_row_read.append(value.encode('utf-8'))
                 else:
                     current_row_read.append(value)
-            if len(current_row_read) > 0 or args.with_empty_rows:
+            if row_not_empty or args.with_empty_rows:
                 rows.append(current_row_read)
             if row_process_number >= int(args.size):
                 break
